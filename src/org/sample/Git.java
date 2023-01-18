b82425a0a8dd43dcfd28424a9a09a48e2b6f7e74
shahulsf <shahulrockzz.h@gmail.com>
2023-01-17 21:20:30 -0800

    done

---
+++ src/org/sample/Git.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class Git {
+	
+	public static void main(String[] args) {
+		
+		String s = "java programming";
+		int a =s.length();
+		System.out.println(a);
+		
+	}
+	
+
+}
